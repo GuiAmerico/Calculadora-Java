5e6b29b4c33a09b71685ae1a72d76314064c6310
GuiAmerico <guilhermao127@gmail.com>
2022-08-11 19:39:11 -0300

    primeira parte do projeto

---
+++ calculadora/src/br/com/guilherme/calc/visao/Display.java
@@ -0,0 +1,9 @@
+package br.com.guilherme.calc.visao;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Display  extends JPanel{
+	public Display() {
+	}
+}
---
+++ calculadora/src/br/com/guilherme/calc/visao/Teclado.java
@@ -0,0 +1,9 @@
+package br.com.guilherme.calc.visao;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Teclado extends JPanel{
+	public Teclado() {
+	}
+}
